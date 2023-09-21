0671d9da3f1a0f26b73681144e44f84c8aebec3b
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2023-09-21 23:09:48 +0900

    useArray hook (#2)
    * CustomHeadingコンポーネント作成
    
    * Arrayコンポーネント作成
    
    * useArray hookを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Container, Heading } from "@chakra-ui/react";
+import { ArrayComponent } from "./hooks/useArray/ArrayComponent";
+
 function App() {
-  return <p>this is custom hook playground</p>;
+  return (
+    <Container w="2xl" pt="7">
+      <Heading as="h1" size="lg" pb="5">
+        custom hook playground.
+      </Heading>
+
+      <ArrayComponent />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/hooks/useArray/ArrayComponent.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, Container, Text } from "@chakra-ui/react";
+import useArray from "./useArray";
+import { CustomHeading } from "../../components/CustomHeading";
+
+export const ArrayComponent = () => {
+  const { array, set, push, remove, filter, update, clear } = useArray<number>([
+    1, 2, 3, 4, 5, 6,
+  ]);
+  return (
+    <>
+      <CustomHeading title={"useArray"} />
+
+      <Box
+        padding={4}
+        backgroundColor={"whitesmoke"}
+        borderRadius={"md"}
+        mb={3}
+      >
+        <Text fontSize={"xl"}>{array.join(", ")}</Text>
+      </Box>
+      <Container display={"flex"} flexDirection={"column"} gap={2}>
+        <Button onClick={() => push(7)}>Add 7</Button>
+        <Button onClick={() => update(1, 9)}>Change Second Element To 9</Button>
+        <Button onClick={() => remove(1)}>Remove Second Element</Button>
+        <Button onClick={() => filter((n) => n < 3)}>
+          Keep Numbers Less Than 4
+        </Button>
+        <Button onClick={() => set([1, 2])}>Set To 1, 2</Button>
+        <Button onClick={clear}>Clear</Button>
+      </Container>
+    </>
+  );
+};
