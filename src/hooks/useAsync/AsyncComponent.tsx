5b6a65d6d08de47e604e3dae5f79c7aa48566c4c
kca2250 <2250katok@gmail.com>
2024-03-10 21:26:37 +0900

    軽微な修正

--- src/hooks/useAsync/AsyncComponent.tsx
+++ src/hooks/useAsync/AsyncComponent.tsx
@@ -1,20 +1,63 @@
+import { useState } from "react";
 import useAsync from "./useAsync";
+import { CollapseSection } from "../../components/CollapseSection";
 
 export default function AsyncComponent() {
-  const { loading, error, value } = useAsync<string>(() => {
+  const [fire, setFire] = useState<boolean>(false);
+  const [active, setActive] = useState<boolean>(true);
+  const { loading, error, value } = useAsync<string>(() =>
+    asyncDisplayString()
+  );
+
+  const clickFire = () => {
+    setFire(true);
+    setActive(!active);
+  };
+
+  function asyncDisplayString(): Promise<string> {
     return new Promise((resolve, reject) => {
-      const success = false;
       setTimeout(() => {
-        success ? resolve("Hi") : reject("Error");
+        fire
+          ? resolve("着火オンファイヤァァァァ")
+          : reject("着火しませんでした");
       }, 1000);
     });
-  });
+  }
 
   return (
-    <div>
-      <div>Loading: {loading.toString()}</div>
-      <div>{error ? error.message : undefined}</div>
-      <div>{value}</div>
-    </div>
+    <CollapseSection title="useAsync">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <div>{error ? error.message : undefined}</div>
+          <div>{value === undefined ? "着火しますか？" : value}</div>
+        </div>
+        <button
+          style={
+            active
+              ? {
+                  outline: "1px solid #ff4500",
+                  padding: "4px 8px",
+                  borderRadius: "4px",
+                  color: "#ff4500",
+                }
+              : {
+                  backgroundColor: "#ff4500",
+                  color: "whitesmoke",
+                  padding: "4px 8px",
+                  borderRadius: "4px",
+                }
+          }
+          onClick={clickFire}
+        >
+          {active ? "ignite" : "ignited"}
+        </button>
+      </div>
+    </CollapseSection>
   );
 }
