157943bc00c095dc240e6d32b7d5cf3f5b9c39ff
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2024-03-11 21:41:15 +0900

    Feature/create use async (#4)
    * 非同期hooksを作成
    
    * 非同期hookを利用したコンポーネントを作成
    
    * 軽微な修正
    
    * useAsyncコンポーネントを配置
    
    * 軽微な修正

---
+++ src/hooks/useAsync/AsyncComponent.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import useAsync from "./useAsync";
+import { CollapseSection } from "../../components/CollapseSection";
+
+export default function AsyncComponent() {
+  const [fire, setFire] = useState<boolean>(false);
+  const [active, setActive] = useState<boolean>(true);
+  const { error, value } = useAsync<string>(() => asyncDisplayString());
+
+  const clickFire = () => {
+    setFire(true);
+    setActive(false);
+  };
+
+  function asyncDisplayString(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        fire
+          ? resolve("着火オンファイヤァァァァ")
+          : reject("着火しませんでした");
+      }, 1000);
+    });
+  }
+
+  return (
+    <CollapseSection title="useAsync">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <div>{error ? error.message : undefined}</div>
+          <div>{value === undefined ? "着火しますか？" : value}</div>
+        </div>
+        <button
+          style={
+            active
+              ? {
+                  outline: "1px solid #ff4500",
+                  padding: "4px 8px",
+                  borderRadius: "4px",
+                  color: "#ff4500",
+                }
+              : {
+                  backgroundColor: "#ff4500",
+                  color: "whitesmoke",
+                  padding: "4px 8px",
+                  borderRadius: "4px",
+                }
+          }
+          onClick={clickFire}
+        >
+          {active ? "点火" : "点火済"}
+        </button>
+      </div>
+    </CollapseSection>
+  );
+}
